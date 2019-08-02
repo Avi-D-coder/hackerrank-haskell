644b01a246418bbc75e2ce5a34ad1e3bfcb659a5
Avi ד <avi.the.coder@gmail.com>
2019-08-02 02:20:25 -0400

    Max non adjacent subsets

--- app/Main.hs
+++ app/Main.hs
@@ -1,39 +1,51 @@
-{-# LANGUAGE FlexibleInstances, UndecidableInstances, DuplicateRecordFields #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Main where
 
-import Control.Monad
-import Data.Array
-import Data.Bits
-import Data.List
-import Data.List.Split
+import           Control.Monad
+
+import           Data.Array
+import           Data.Bits
+
+-- import Data.List
+-- import Data.List.Split
 -- import Data.Set
-import Debug.Trace
-import System.Environment
-import System.IO
-import System.IO.Unsafe
+-- import Debug.Trace
+import           System.Environment
+import           System.IO
+import           System.IO.Unsafe
 
--- Complete the countingValleys function below.
-countingValleys:: Int -> String -> Int
-countingValleys n = fst . foldl' newAlt (0, 0)
-    where
-        newAlt (val, alt) step
-            | alt == -1 && step == 'U' = (val + 1, 0)
-            | step == 'D' = (val, alt - 1)
-            | step == 'U' = (val, alt + 1)
+-- -2 3 2 4 5
+-- Complete the maxSubsetSum function below.
+maxSubsetSum :: [Int] -> Int
+maxSubsetSum = uncurry max . foldr f (0, 0)
 
-main :: IO()
+f :: Int -> (Int, Int) -> (Int, Int)
+f n (p', p) = (max p' p, max n $ p' + n)
+
+readMultipleLinesAsStringArray :: Int -> IO [String]
+readMultipleLinesAsStringArray 0 = return []
+readMultipleLinesAsStringArray n = do
+    line <- getLine
+    rest <- readMultipleLinesAsStringArray (n - 1)
+    return (line : rest)
+
+main :: IO ()
 main = do
-    stdout <- getEnv "OUTPUT_PATH"
-    fptr <- openFile stdout WriteMode
+    stdout  <- getEnv "OUTPUT_PATH"
+    fptr    <- openFile stdout WriteMode
 
-    n <- readLn :: IO Int
+    n       <- readLn :: IO Int
 
-    s <- getLine
+    arrTemp <- getLine
 
-    let result = countingValleys n s
+    let arr = map (read :: String -> Int) . words $ arrTemp
 
-    hPutStrLn fptr $ show result
+    let res = maxSubsetSum arr
+
+    hPutStrLn fptr $ show res
 
     hFlush fptr
     hClose fptr
