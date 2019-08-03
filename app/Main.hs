44d62d984fcef8727657acef151bd4a950bfcd7b
Avi ד <avi.the.coder@gmail.com>
2019-08-03 15:57:20 -0400

    ST memoization

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,11 @@
-{-# LANGUAGE DuplicateRecordFields #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
-
 module Main where
 
 import           Control.Monad
+import           Control.Monad.ST
 
 import           Data.Char
+import qualified Data.Map.Strict               as M
+import           Data.STRef
 
 import           System.Environment
 import           System.IO
@@ -14,37 +13,49 @@ import           System.IO.Unsafe
 
 -- Complete the abbreviation function below.
 abbreviation :: String -> String -> String
-abbreviation a b = if abb a b then "YES" else "NO"
+abbreviation a b = if abbM a b then "YES" else "NO"
 
-abb :: String -> String -> Bool
-abb a  [] = all isLower a
-abb [] _  = False
-abb a b | ha == hb         = abb ta tb
-        | toUpper ha == hb = abb ta tb || abb ta b
-        | isUpper ha       = False
-        | otherwise        = abb ta b
+abbM :: String -> String -> Bool
+abbM a b = runST $ do
+  m <- newSTRef M.empty
+  abb a b m
+
+abb :: String -> String -> STRef s (M.Map (String, String) Bool) -> ST s Bool
+abb a  [] _ = return $ all isLower a
+abb [] _  _ = return False
+abb a  b  m = do
+  mm <- readSTRef m
+  case M.lookup (a, b) mm of
+    Just memo -> return memo
+    Nothing   -> do
+      r <- recur
+      modifySTRef m $ M.insert (a, b) r
+      recur
  where
   ha : ta = a
 
   hb : tb = b
 
+  recur | ha == hb         = abb ta tb m
+        | toUpper ha == hb = (||) <$> abb ta tb m <*> abb ta b m
+        | isUpper ha       = return False
+        | otherwise        = abb ta b m
 
 main :: IO ()
 main = do
-    stdout <- getEnv "OUTPUT_PATH"
-    fptr   <- openFile stdout WriteMode
+  stdout <- getEnv "OUTPUT_PATH"
+  fptr   <- openFile stdout WriteMode
 
-    q      <- readLn :: IO Int
+  q      <- readLn :: IO Int
 
-    forM_ [1 .. q] $ \q_itr -> do
-        a <- getLine
+  forM_ [1 .. q] $ \q_itr -> do
+    a <- getLine
 
-        b <- getLine
+    b <- getLine
 
-        let result = abbreviation a b
+    let result = abbreviation a b
 
-        hPutStrLn fptr result
+    hPutStrLn fptr result
 
-    hFlush fptr
-    hClose fptr
-
+  hFlush fptr
+  hClose fptr
