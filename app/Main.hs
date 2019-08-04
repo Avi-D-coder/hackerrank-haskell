3d95c66854007815b62aeae8a100f5e1abaa2897
Avi ד <avi.the.coder@gmail.com>
2019-08-03 23:15:44 -0400

    MemoTrie Text

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@
 
 module Main where
 
+import           Control.Arrow                  ( first )
 import           Control.Monad
 
 import           Data.Char
@@ -18,39 +19,53 @@ import           System.Environment
 import           System.IO
 import           System.IO.Unsafe
 
+enum' :: (HasTrie a) => (a -> a') -> (a :->: b) -> [(a', b)]
+enum' f = (fmap . first) f . enumerate
+
+instance HasTrie T.Text where
+  newtype (T.Text :->: b) = TextTrie (String :->: b)
+
+  trie f = TextTrie (trie (f . T.pack))
+
+  untrie (TextTrie t) = untrie t . T.unpack
+
+  enumerate (TextTrie t) = enum' T.pack t
+
 -- Complete the abbreviation function below.
 abbreviation :: String -> String -> String
-abbreviation a b = if abb a b then "YES" else "NO"
+abbreviation a b = if abb (T.pack a) (T.pack b) then "YES" else "NO"
 
-abb :: String -> String -> Bool
-abb a  [] = all isLower a
-abb [] _  = False
+abb :: T.Text -> T.Text -> Bool
+abb a  "" = T.all isLower a
+abb "" _  = False
 abb a b | ha == hb         = memo2 abb ta tb
         | toUpper ha == hb = memo2 abb ta tb || memo2 abb ta b
         | isUpper ha       = False
         | otherwise        = memo2 abb ta b
  where
-  ha : ta = a
+  ha = T.head a
 
-  hb : tb = b
+  ta = T.tail a
 
+  hb = T.head b
+
+  tb = T.tail b
 
 main :: IO ()
 main = do
-    stdout <- getEnv "OUTPUT_PATH"
-    fptr   <- openFile stdout WriteMode
+  stdout <- getEnv "OUTPUT_PATH"
+  fptr   <- openFile stdout WriteMode
 
-    q      <- readLn :: IO Int
+  q      <- readLn :: IO Int
 
-    forM_ [1 .. q] $ \q_itr -> do
-        a <- getLine
+  forM_ [1 .. q] $ \q_itr -> do
+    a <- getLine
 
-        b <- getLine
+    b <- getLine
 
-        let result = abbreviation a b
+    let result = abbreviation a b
 
-        hPutStrLn fptr result
+    hPutStrLn fptr result
 
-    hFlush fptr
-    hClose fptr
-
+  hFlush fptr
+  hClose fptr
